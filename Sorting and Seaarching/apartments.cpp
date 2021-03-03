cf5e860a0cdf0979460f4d2b01fe53f783990d22
ssr <sheetalsinghnew@gmail.com>
2021-03-03 23:09:19 +0530

    apartment

--- Sorting and Seaarching/apartments.cpp
+++ Sorting and Seaarching/apartments.cpp
@@ -1,29 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef unsigned long long int ulli;
-#define for(i,a,b) for(int i=a;i<b;i++)
+typedef long long int ulli;
+#define for(i,a,b) for(ulli i=a;i<b;i++)
 
 int main()
 {
 ios::sync_with_stdio(0);
 cin.tie(0);
 
-ulli n,x;
-cin>>n>>x;
+ulli n,m,k;
+cin>>n>>m>>k;
 ulli a[n];
+ulli b[m];
+ulli c[m]={0};
 for(i,0,n)
 {
     cin>>a[i];
 }
+for(i,0,m)
+{
+   cin>>b[i];
+}
+sort(a,a+n);
+sort(b,b+m);
+int ans=0;
+int z=0;
 for(i,0,n)
 {
-    for(j,i+1,n)
+    for(j,z,m)
     {
-        if(a[i]+a[j]==x)
+
+        if((b[j]>=a[i]-k && b[j]<=a[i]+k) && c[j]==0)
         {
-            cout<<i+1<<" "<<j+1;
-            return 0;
+            ans++;
+            c[j] = 1;
+             z = j+1;
+            break;
         }
+        else
+        if(b[j]>a[i]+k)
+            break;
     }
 }
+cout<<ans;
 return 0;}
